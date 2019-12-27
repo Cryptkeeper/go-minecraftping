4cac742ccca14cb07a17d6586107f008aa020ad9
Crypt <hello@cryptkpr.me>
2019-12-27 12:11:04 -0600

    LatestProtocolVersion = 575/Minecraft Java Edition 1.15.1

--- minecraftping.go
+++ minecraftping.go
@@ -19,9 +19,9 @@ const (
 
 	// LatestProtocolVersion is the latest released Minecraft Java Edition protocol version.
 	// This field is manually updated and may be out of date.
-	// Corresponding Minecraft version: 1.14.4
+	// Corresponding Minecraft version: 1.15.1
 	// More protocol versions: https://wiki.vg/Protocol_version_numbers
-	LatestProtocolVersion = 498
+	LatestProtocolVersion = 575
 )
 
 // requestPacket is a cached copy of the Request packet.
